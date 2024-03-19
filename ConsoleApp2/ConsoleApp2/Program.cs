7d1fbb05846fcc48fffc8ababbd1df1d5f6f7a15
COOLRUNERuni <s27237@pjwstk.edu.pl>
2024-03-19 01:17:37 +0100

    Task is done

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -8,8 +8,22 @@ class MyClass
     {
         int[] arr = { 1, 2, 3, 4, 5, 56, 4 };
         Console.WriteLine(calcAvg(arr));
+        Console.WriteLine(calcMax(arr));
     }
 
+    public static int calcMax(int[] a)
+    {
+        int max = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] > max)
+            {
+                max = a[i];
+            }
+        }
+
+        return max;
+    }
 
     public static int calcAvg(int[] a)
     {
